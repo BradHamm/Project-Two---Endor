717b205f3adcc56ee1fb8239d9c60f5ff61d174a
Neta2393 <benetheahardin@yahoo.com>
2023-07-15 09:03:29 -0700

    Added background color to buttons

--- controllers/like.js
+++ controllers/like.js
@@ -7,11 +7,11 @@ var upCount = localStorage.getItem('thumbsUpCount') || 0;
 var downCount = localStorage.getItem('thumbsDownCount') || 0;
 
 // Added event listener to communicate on click to add a number to the like and dislike count
-// Saved the previous likes and dislikes to the local storage so they still populate even when the page is refreshed
 thumbsUpButton.addEventListener('click', function() {
     upCount++;
     thumbsUpCount.innerText = upCount;
     localStorage.setItem('thumbsUpCount', upCount);
+ // Saved the previous likes and dislikes to the local storage so they still populate even when the page is refreshed   
 });
 
 thumbsDownButton.addEventListener('click', function() {
