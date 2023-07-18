9d2f7ad12ae6393836d0136fcbea8ed66672bdc5
JoaquinGodina0 <godinajoaquin6@gmail.com>
2023-07-17 20:02:41 -0700

    added css and javascript

--- login.js
+++
@@ -1,7 +0,0 @@
-// Function to handle logout
-function logout() {
-    // Redirects to the logout page
-    window.location.href = 'logout.html';
-}
-// Calls logout function when the page loads
-window.onload = logout;
