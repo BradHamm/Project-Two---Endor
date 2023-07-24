8f88d66b8b8f4a536b796a97dc4aa3283b8e00b8
itsa-me-dea <wachadea@gmail.com>
2023-07-23 21:59:57 -0700

    COMPLETE overhaul, backend needs to be revised

--- models/index.js
+++ models/index.js
@@ -1,12 +1,13 @@
 const User = require('./User');
-const Posts = require('./Posts');
-const Tags = require('./Tags');
-const FriendsList = require('./FriendsList');
+const Project = require('./Project');
 
-FriendsList.belongsTo(User, { foreignKey: 'userId', as: 'user' });
-FriendsList.belongsTo(User, { foreignKey: 'friendId', as: 'friend' });
-Posts.belongsTo(User, { foreignKey: 'author_id' });
-// Posts.belongsToMany(Tags, { through: 'PostTags', foreignKey: 'post_id' });
-// Tags.belongsToMany(Posts, { through: 'PostTags', foreignKey: 'tag_id' });
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
-module.exports = { User, Posts, Tags, FriendsList}
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Project };
--- server.js
+++ server.js
@@ -1,44 +1,46 @@
-require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const Sequelize = require('./config/connection');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const routes = require('./routes/routes');
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: Sequelize ,
-  }),
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
-
+// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(routes);
 
-// app.use(require(routes));
-
-app.get('/', (req, res) => {
-  // Redirect to the homepage
-  res.redirect('/homepage');
-});
-
-Sequelize.sync({ force: true }).then(() => { //toggle to false
-  app.listen(PORT, () => console.log('Now listening'));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on http://localhost:3001/'));
 });
